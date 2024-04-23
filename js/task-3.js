ef52093e7a212881733e0f32f1f6b5c33450956f
Myroslav Raks <raksmyroslav@gmail.com>
2024-04-23 20:05:40 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,28 @@
-function getElementWidth(content, padding, border) {
-    content = parseFloat(content);
-    padding = parseFloat(padding);
-    border = parseFloat(border);
-    return content + padding * 2 + border * 2;
-}
+// function getElementWidth(content, padding, border) {
+//     content = parseFloat(content);
+//     padding = parseFloat(padding);
+//     border = parseFloat(border);
+//     return content + padding * 2 + border * 2;
+// }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+// function getRandomNumber(min, max) {
+//     return Math.random() * (max - min + 1) + min;
+// }
+
+// const randomNumber = getRandomNumber(1, 20);
+// console.log(randomNumber);
+
+// ? Напиши функцію, яка буде перевіряти пароль у користувача. Запроси пароль у користувача через promt
+
+// const password = prompt("Enter your password");
+// function checkPassword(value) {
+//     const password = "Admin";
+//     return value === password;
+// }
+
+// const isChecked = checkPassword(password);
+// console.log(isChecked);
